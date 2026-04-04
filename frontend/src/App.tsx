fbc60422e701e51ef71253089ee3416f03c58a36
Meganathan N <megacode26@gmail.com>
2026-04-04 22:02:10 +0530

    Initial Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-[100vh]  flex bg-black justify-center items-center">
+      <h1 className="text-white text-2xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
